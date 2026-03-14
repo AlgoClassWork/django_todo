33ab9f30d21baa6f3cd0415183f71ec22268e163
AlgoClassWork <kyxec.play@gmail.com>
2026-03-14 11:28:39 +0600

    Add files via upload

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    tasks = Task.objects.all()
+    return render(request, 'index.html', {'tasks' : tasks} )
